0fc363cca7306bb5c131eccb289e6aaa440143a3
Lalit Kumar <kumarlalitss166@gmail.com>
2025-05-27 16:57:12 +0530

    System-Default Theme and Theme Saving on Reload Feature Added

--- Web Development/P1_Theme_Toggler/main/script.js
+++ Web Development/P1_Theme_Toggler/main/script.js
@@ -1,10 +1,29 @@
 const theme_toggler = document.getElementById('toggle_theme')
-console.log(theme_toggler)
-
 const bg = document.querySelector('body')
     
 let theme_light = true
 
+//Check saved theme in localStorage and apply it on page load
+let savedTheme = localStorage.getItem('theme');
+if (!savedTheme){
+    //Checks for system default theme
+    savedTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.setItem('theme', savedTheme);
+}
+if (savedTheme === 'dark'){
+    bg.style.backgroundColor = 'black';
+    bg.style.color = 'white';
+    theme_toggler.style.backgroundColor = 'rgb(87, 85, 85)';
+    theme_light = false;
+}
+else{
+    bg.style.backgroundColor = 'white';
+    bg.style.color = 'black';
+    theme_toggler.style.backgroundColor = 'white';
+    theme_light = true;
+}
+
+//Event Listener on toggle
 theme_toggler.addEventListener('click', ()=>{
     //Toggle Dark
     if(theme_light){     
@@ -12,6 +31,9 @@ theme_toggler.addEventListener('click', ()=>{
         bg.style.color = 'white';
         theme_toggler.style.backgroundColor = 'rgb(87, 85, 85)';
         theme_light = false
+
+        localStorage.setItem('theme', 'dark')
+        
     }
     //Toggle Light
     else{
@@ -19,5 +41,7 @@ theme_toggler.addEventListener('click', ()=>{
         bg.style.color = 'black';
         theme_toggler.style.backgroundColor = 'white'
         theme_light = true
+
+        localStorage.setItem('theme', 'light')
     }
 })
