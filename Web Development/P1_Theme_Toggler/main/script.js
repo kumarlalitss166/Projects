129dd7b2e9250614ec01329d6b2be08921fc2527
Lalit Kumar <kumarlalitss166@gmail.com>
2025-05-28 20:44:27 +0530

    Theme Button splitted

--- Web Development/P1_Theme_Toggler/main/script.js
+++ Web Development/P1_Theme_Toggler/main/script.js
@@ -11,23 +11,26 @@ if (!savedTheme){
     localStorage.setItem('theme', savedTheme);
 }
 if (savedTheme === 'dark'){
-    bg.style.backgroundColor = 'black';
+    bg.style.backgroundColor = 'rgb(31, 31, 31)';
     bg.style.color = 'white';
     theme_toggler.style.backgroundColor = 'rgb(87, 85, 85)';
     theme_light = false;
 }
 else{
     bg.style.backgroundColor = 'white';
-    bg.style.color = 'black';
+    bg.style.color = 'rgb(31, 31, 31)';
     theme_toggler.style.backgroundColor = 'white';
     theme_light = true;
 }
 
 //Event Listener on toggle
-theme_toggler.addEventListener('click', ()=>{
+theme_toggler.addEventListener('click', (event)=>{
+
+    console.log(event.target.id, "theme applied.");
+
     //Toggle Dark
     if(theme_light){     
-        bg.style.backgroundColor = 'black'
+        bg.style.backgroundColor = 'rgb(31, 31, 31)'
         bg.style.color = 'white';
         theme_toggler.style.backgroundColor = 'rgb(87, 85, 85)';
         theme_light = false
@@ -38,7 +41,7 @@ theme_toggler.addEventListener('click', ()=>{
     //Toggle Light
     else{
         bg.style.backgroundColor = 'white'
-        bg.style.color = 'black';
+        bg.style.color = 'rgb(31, 31, 31)';
         theme_toggler.style.backgroundColor = 'white'
         theme_light = true
 
