464d85787fe6404ad671bb8573dec7da9113e060
Lalit Kumar <kumarlalitss166@gmail.com>
2025-05-28 22:15:34 +0530

    Added Theme Button Movement

--- Web Development/P1_Theme_Toggler/main/script.js
+++ Web Development/P1_Theme_Toggler/main/script.js
@@ -1,15 +1,22 @@
 const theme_toggler = document.getElementById('toggle_theme')
 const bg = document.querySelector('body')
+const circle = document.getElementById('circle');
     
 let theme_light = true
 
-//Check saved theme in localStorage and apply it on page load
+//Loading saved theme and position
 let savedTheme = localStorage.getItem('theme');
+let savedPos = localStorage.getItem('Cpos');
+
+//Default to system theme
+let Cpos = localStorage.getItem('Cpos')
 if (!savedTheme){
     //Checks for system default theme
     savedTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     localStorage.setItem('theme', savedTheme);
 }
+
+//Apply saved theme
 if (savedTheme === 'dark'){
     bg.style.backgroundColor = 'rgb(31, 31, 31)';
     bg.style.color = 'white';
@@ -21,6 +28,35 @@ else{
     bg.style.color = 'rgb(31, 31, 31)';
     theme_toggler.style.backgroundColor = 'white';
     theme_light = true;
+}
+
+//Apply saved circle postion or set Default
+if (savedPos){
+    circle.style.left = savedPos;
+}
+else{
+    circle.style.left = '5px';
+    localStorage.setItem('Cpos', '5px');
+}
+
+//Button click handler
+function moveCircle(event) {
+    const circle = document.getElementById('circle');
+    const clickX = event.offsetX;
+    const middle = theme_toggler.offsetWidth / 2;
+    let when_light;
+    let when_dark;
+    if (clickX < middle) {
+        // Move to left
+        circle.style.left = "5px";
+        localStorage.setItem('Cpos', '5px')
+    } 
+    else {
+        // Move to right
+        const rightPos = (theme_toggler.offsetWidth - circle.offsetWidth - 5) + 'px';
+        circle.style.left = rightPos;
+        localStorage.setItem('Cpos', rightPos);
+    }
 }
 
 //Event Listener on toggle
@@ -48,3 +84,4 @@ theme_toggler.addEventListener('click', (event)=>{
         localStorage.setItem('theme', 'light')
     }
 })
+
