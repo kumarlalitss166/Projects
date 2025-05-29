279e4ab9564db717ebbaf60db93d9fed7b7584e4
Lalit Kumar <kumarlalitss166@gmail.com>
2025-05-29 15:48:03 +0530

    Theme Toggler Project completed

--- Web Development/P1_Theme_Toggler/main/script.js
+++ Web Development/P1_Theme_Toggler/main/script.js
@@ -1,7 +1,8 @@
 const theme_toggler = document.getElementById('toggle_theme')
 const bg = document.querySelector('body')
 const circle = document.getElementById('circle');
-    
+const header_border = document.querySelector('.header_div')
+
 let theme_light = true
 
 //Loading saved theme and position
@@ -17,27 +18,30 @@ if (!savedTheme){
 //Apply saved theme and set circle position/color accordingly
 function applyTheme(theme) {
     if (theme === 'dark'){
-        bg.style.backgroundColor = 'rgb(31, 31, 31)';
+        bg.style.backgroundColor = 'rgb(19, 19, 19)';
         bg.style.color = 'white';
-        theme_toggler.style.backgroundColor = 'rgb(87, 85, 85)';
+        theme_toggler.style.backgroundColor = 'rgb(105, 105, 105)';
         theme_light = false;
-        
-        // Circle should be white in dark theme and positioned right
-        circle.style.backgroundColor = 'white';
-        circle.style.borderColor = 'white';
+        //Circle Left
         const rightPos = (theme_toggler.offsetWidth - circle.offsetWidth - 5) + 'px';
         circle.style.left = rightPos;
+        circle.style.backgroundColor = 'white';
+        circle.style.border = 'none';
+        circle.style.transition = 'background-color: 1s ease'
+        
+        header_border.style.border = 'solid white 2px'    
     }
     else{
         bg.style.backgroundColor = 'white';
         bg.style.color = 'rgb(31, 31, 31)';
         theme_toggler.style.backgroundColor = '#ddd';
         theme_light = true;
-        
-        // Circle should be black in light theme and positioned left
-        circle.style.backgroundColor = 'black';
-        circle.style.borderColor = 'black';
+        //Circle Right
         circle.style.left = '5px';
+        circle.style.backgroundColor = 'black'
+        circle.style.border = 'solid black 1px'
+        circle.style.transition = 'background-color: 1s ease'
+        header_border.style.border = 'solid black 2px'        
     }
 }
 
@@ -52,10 +56,18 @@ theme_toggler.addEventListener('click', (event)=>{
     if(theme_light){     
         applyTheme('dark');
         localStorage.setItem('theme', 'dark');
+        circle.style.backgroundColor = 'white';
+        circle.style.border = 'none';
+        circle.style.transition = 'background-color: 1s ease'
+        header_border.style.border = 'solid white 2px'
     }
     //Toggle Light
     else{
         applyTheme('light');
         localStorage.setItem('theme', 'light');
+        circle.style.backgroundColor = 'black'
+        circle.style.border = 'solid black 1px'
+        circle.style.transition = 'background-color: 1s ease'
+        header_border.style.border = 'solid black 2px'
     }
 })
